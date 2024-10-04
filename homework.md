863c329f9b5fe97351c2998a90e187063c981b52
Suraj-Vishwas <vishwassuraj165@gmail.com>
2024-10-04 12:16:54 +0530

    multiple-route handlers

--- homework.md
+++ homework.md
@@ -21,3 +21,20 @@
 
 - Play with routes and route extensions ex. /hello, /, /hello/3, /xyz
 - Order of the routes matter a lot
+
+
+ 
+- Install postman app and make a workspace/collection > test API call
+- write logic to handle GET,POST,DELETE,PATCH PAI calls and test them on Postman
+
+
+- Explore routing and use of ?, +, *, () in the routes
+- Use of regex in routes /a/ , /.*fly$/
+- Reading the query params in the routes
+- Reading the dynamic routes
+
+
+- Multiple Route Handlers - Play with the code
+- next()
+- next function and errors along with res.send()
+- app.use("/route", rH, [rH2, rH3], rH4, rH5);
--- src/app.js
+++ src/app.js
@@ -3,56 +3,36 @@ const app = express();
 
 // request handler
 
-// // and here the b is optional because of ? means you can write ac or abc
-// app.get("/ab?c", (req,res) => {
-//     res.send({firstName:"suraj", lastName:"vishwas"});
-// })
+// app.use("/route", rH, [rH2, rH3], rH4, rH5);
 
-// app.get("/a(bc)+d", (req,res) => {
-//     res.send({firstName:"suraj", lastName:"vishwas"});
-// })
-
-
-// // ====>  /abbbbbcd  here you can start with ab and end with cd and between them 
-// // you can write as much as b
-// app.get("/ab+cd", (req,res) => {
-//     res.send({firstName:"suraj", lastName:"vishwas"});
-// })
-
-// // ====>  /abdskhfkshgcd  here you can start with ab and end with cd and between them 
-// // you can write anything
-// app.get("/ab*cd", (req,res) => {
-//     res.send({firstName:"suraj", lastName:"vishwas"});
-// })
-
-
-
-
-// you can also pass here regex
-// // here it will true if in your path a is exist
-// app.get(/a/,(req,res)=>{
-//     res.send({firstName:"suraj", lastName:"vishwas"});    
-// })
-
-// // here it will true if  your path end with fly
-// app.get(/.*fly$/,(req,res)=>{
-//         res.send({firstName:"suraj", lastName:"vishwas"});    
-// })
-
-// // http://localhost:4000/user?userId=suraj&password=xyz
-// app.get("/user",(req,res)=>{
-//     console.log(req.query);
-//     res.send({firstName:"suraj", lastName:"vishwas"});    
-// })
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"suraj", lastName:"vishwas"});    
-})
-
-
-
-
-
+app.get(
+    "/user", 
+    (req, res, next) => {
+        console.log("Handling the route handler 1");
+        // res.send("1st Response!")
+        next();
+    },
+   [ (req, res, next) => {
+        console.log("Handling the route handler 2");
+        // res.send("2nd Response!")
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route handler 3");
+        res.send("3rd Response!")
+        // next();
+    }],
+    (req, res, next) => {
+        console.log("Handling the route handler 4");
+        // res.send("4th Response!")
+        // next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route handler 5");
+        res.send("5th Response!")
+        // next();
+    }
+    )
 
 app.listen(4000, ()=>{
     console.log("Server is successfully listening on port 4000"); 
