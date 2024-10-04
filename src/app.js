307402f3865dbd141006b03d526877efed2b8f6b
Suraj-Vishwas <vishwassuraj165@gmail.com>
2024-10-04 11:21:39 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,42 +3,56 @@ const app = express();
 
 // request handler
 
-app.get("/user", (req,res) => {
-    res.send("getting data successfully from DB...");
-})
-app.post("/user", (req,res) => {
-    // create entries in db
-    // res.send("post data successfully in DB...");
-    res.send({firstName:"suraj", lastName:"vishwas"});
-})
-app.delete("/user", (req,res) => {
-    res.send("Delete data from DB...");
-})
-
-
-// app.use("/", (req,res) => {
-//     res.send("Hello from the server...");
+// // and here the b is optional because of ? means you can write ac or abc
+// app.get("/ab?c", (req,res) => {
+//     res.send({firstName:"suraj", lastName:"vishwas"});
 // })
 
-app.use("/hello/3", (req,res) => {
-    res.send("Hello Hi Namaste Hello Hi Namaste...");
-})
-
-app.use("/hello", (req,res) => {
-    res.send("Hello Hi Namaste...");
-})
-
-// app.use("/hello/3", (req,res) => {
-//     res.send("Hello Hi Namaste Hello Hi Namaste...");
+// app.get("/a(bc)+d", (req,res) => {
+//     res.send({firstName:"suraj", lastName:"vishwas"});
 // })
 
-app.use("/test", (req,res) => {
-    res.send("This is testing phase...");
+
+// // ====>  /abbbbbcd  here you can start with ab and end with cd and between them 
+// // you can write as much as b
+// app.get("/ab+cd", (req,res) => {
+//     res.send({firstName:"suraj", lastName:"vishwas"});
+// })
+
+// // ====>  /abdskhfkshgcd  here you can start with ab and end with cd and between them 
+// // you can write anything
+// app.get("/ab*cd", (req,res) => {
+//     res.send({firstName:"suraj", lastName:"vishwas"});
+// })
+
+
+
+
+// you can also pass here regex
+// // here it will true if in your path a is exist
+// app.get(/a/,(req,res)=>{
+//     res.send({firstName:"suraj", lastName:"vishwas"});    
+// })
+
+// // here it will true if  your path end with fly
+// app.get(/.*fly$/,(req,res)=>{
+//         res.send({firstName:"suraj", lastName:"vishwas"});    
+// })
+
+// // http://localhost:4000/user?userId=suraj&password=xyz
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({firstName:"suraj", lastName:"vishwas"});    
+// })
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"suraj", lastName:"vishwas"});    
 })
 
-// app.use("/", (req,res) => {
-//     res.send("Hello Suraj...");
-// })
+
+
+
+
 
 app.listen(4000, ()=>{
     console.log("Server is successfully listening on port 4000"); 
